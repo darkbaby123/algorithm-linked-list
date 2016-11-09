a04957717c30b0148200197f69ac23151d03120d
David Chen <darkbaby123@gmail.com>
2016-11-09 15:40:48 +0800

    feat: implement 05-sorted-insert and tests

---
+++ test/05-sorted-insert.test.js
@@ -0,0 +1,53 @@
+const { buildOneTwoThree } = require('../lib/00-utils')
+const { sortedInsert, sortedInsertV2 } = require('../lib/05-sorted-insert')
+
+describe('05-sorted-insert', () => {
+  createTests(sortedInsert)
+  createTests(sortedInsertV2)
+
+  function createTests(fn) {
+    describe(fn.name, () => {
+      it('should be able to handle an empty list.', () => {
+        const list = fn(null, 23)
+        expect(list.data).toEqual(23)
+        expect(list.next).toEqual(null)
+      })
+
+      it('should be able to insert a new node at the head of a list.', () => {
+        const list = fn(buildOneTwoThree(), 0.5)
+        expect(list.data).toEqual(0.5)
+        expect(list.next.data).toEqual(1)
+        expect(list.next.next.data).toEqual(2)
+        expect(list.next.next.next.data).toEqual(3)
+        expect(list.next.next.next.next).toEqual(null)
+      })
+
+      it('should be able to insert a new node at index 1 of a list.', () => {
+        const list = fn(buildOneTwoThree(), 1.5)
+        expect(list.data).toEqual(1)
+        expect(list.next.data).toEqual(1.5)
+        expect(list.next.next.data).toEqual(2)
+        expect(list.next.next.next.data).toEqual(3)
+        expect(list.next.next.next.next).toEqual(null)
+      })
+
+      it('should be able to insert a new node at index 2 of a list.', () => {
+        const list = fn(buildOneTwoThree(), 2.5)
+        expect(list.data).toEqual(1)
+        expect(list.next.data).toEqual(2)
+        expect(list.next.next.data).toEqual(2.5)
+        expect(list.next.next.next.data).toEqual(3)
+        expect(list.next.next.next.next).toEqual(null)
+      })
+
+      it('should be able to insert a new node at tail of a list.', () => {
+        const list = fn(buildOneTwoThree(), 3.5)
+        expect(list.data).toEqual(1)
+        expect(list.next.data).toEqual(2)
+        expect(list.next.next.data).toEqual(3)
+        expect(list.next.next.next.data).toEqual(3.5)
+        expect(list.next.next.next.next).toEqual(null)
+      })
+    })
+  }
+})
