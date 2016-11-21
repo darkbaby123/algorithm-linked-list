b499aa11bbf201d6d309e6b13eb7c20a675c470e
David Chen <darkbaby123@gmail.com>
2016-11-21 13:26:10 +0800

    feat: implement 11-alternating-split loop version with dump node

--- test/11-alternating-split.test.js
+++ test/11-alternating-split.test.js
@@ -1,10 +1,15 @@
 const { Node } = require('../lib/00-utils')
 const { buildNodes } = require('../lib/01-push-and-build-one-two-three')
-const { alternatingSplit, alternatingSplitV2 } = require('../lib/11-alternating-split')
+const {
+  alternatingSplit,
+  alternatingSplitV2,
+  alternatingSplitV3,
+} = require('../lib/11-alternating-split')
 
 describe('11 Alternating Split', () => {
   createTests(alternatingSplit)
   createTests(alternatingSplitV2)
+  createTests(alternatingSplitV3)
 
   function createTests(fn) {
     describe(fn.name, () => {
